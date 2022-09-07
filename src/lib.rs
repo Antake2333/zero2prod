b9ec0552c2a3bd112e13daefee8ca38e5a47f18e
antake2333 <antake2333@gmail.com>
2022-09-07 22:35:48 +0800

    增加订阅略有

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,18 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)
-        .expect("Can listener bind to port 8000")
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)
+    .expect("Can listener bind to port 8000")
+    .run();
     Ok(server)
 }
