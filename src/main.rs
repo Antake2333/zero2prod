a22f45a35dca0b60f0adbe595dea4464a1da414d
antake2333 <antake2333@gmail.com>
2022-09-09 23:52:54 +0800

    替换连接为连接池

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuation;
 use zero2prod::startup::run;
@@ -6,10 +6,10 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuation().expect("Failed to get configuration");
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
