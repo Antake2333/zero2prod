bcfce820c60e56e8278f545af0983ce2e2396856
antake2333 <antake2333@gmail.com>
2022-09-15 22:48:48 +0800

    增加tracing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuation;
 use zero2prod::startup::run;
-use env_logger::Env;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // 初始化日志
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,7 +10,24 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!("Saving new subsciber details in database");
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subcriber.",
+        %request_id,
+        subscriber_email=%form.email,
+        subscriber_name=%form.name,
+    );
+    let _request_span_guard = request_span.enter();
+    tracing::info!(
+        "request_id:{} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+    tracing::info!(
+        "request_id:{} - Saving new subsciber details in database",
+        request_id
+    );
     match sqlx::query!(
         r#"
 INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -25,11 +42,18 @@ VALUES ($1, $2, $3, $4)
     .await
     {
         Ok(_) => {
-            log::info!("New subsciber details have been saved");
+            tracing::info!(
+                "request_id:{} - New subsciber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
-        },
+        }
         Err(e) => {
-            log::error!("Failed to save new subsciber details in database: {:?}", e);
+            tracing::error!(
+                "request_id:{} - Failed to save new subsciber details in database: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
